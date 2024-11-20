3f1537c41004dcc55d9d340d326bf689c35e885c
Paul Lemeilleur <paul.lemeilleur@gmail.com>
2024-11-20 10:42:51 +0100

    Implement functions of Tools class

--- src/tools.ml
+++ src/tools.ml
@@ -2,10 +2,16 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes (gr: a' graph) =
+let clone_nodes (gr: 'a graph) =
   n_fold gr (fun gr' id -> new_node gr' id) empty_graph
 
 
-let gmap _gr _f = assert false
+let gmap (gr: 'a graph) (f: 'a -> 'b) = 
+  let new_graph = clone_nodes gr in
+  e_fold gr (fun g arc -> new_arc g {src=arc.src; tgt=arc.tgt; lbl = (f arc.lbl)}) new_graph
+
 (* Replace _gr and _f by gr and f when you start writing the real function. *)
-...
+
+let add_arc (g : int graph) (id1 : id) (id2 : id) (n : int) =
+  new_arc g {src=id1; tgt=id2; lbl=n}
+
