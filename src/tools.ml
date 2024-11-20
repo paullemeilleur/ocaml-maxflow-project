1c9010e811745316eb772ba97b4849cfa12bcd69
sophielrge <sophie.large@wanadoo.fr>
2024-11-20 10:19:19 +0100

    clone_nodes

--- src/tools.ml
+++ src/tools.ml
@@ -2,7 +2,10 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes _gr = assert false
+let clone_nodes (gr: a' graph) =
+  n_fold gr (fun gr' id -> new_node gr' id) empty_graph
+
+
 let gmap _gr _f = assert false
 (* Replace _gr and _f by gr and f when you start writing the real function. *)
 ...
