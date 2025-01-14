219bfb2c7f29e920e6458d7f53e16ccb1c2d90f3
Paul Lemeilleur <paul.lemeilleur@gmail.com>
2025-01-14 13:02:09 +0100

    Remove unused output files and clean up the dune file; update graph handling in ftest.ml

--- src/ford_fulk.mli
+++ src/ford_fulk.mli
@@ -1,29 +1,6 @@
 open Graph
 open Gfile
 
-type flot =
-  { 
-    (* Capacité *)
-    capa: int ;
-
-    (* Flot *)  
-    flot: int}
-
-
-(* Création d'un graphe flot *)
-val create_flot_graph: int graph -> flot graph;;
-
-(*Graph ecart*)
-val graph_ecart: flot graph -> int graph;;
-
-(* Find path *)
-val find_path: int graph -> id -> id -> id list;;
-
-(* Find minimum value of the path*)
-val min_path_value: int graph -> id list -> int;;
-
-(* Update flot graph *)
-val update_path: int graph -> id list -> int -> int graph;;
 
 (* Ford-Fulkerson *)
 val ford_fulkerson: id graph -> id -> id -> (path graph -> path -> unit)-> unit;;
--- src/ftest.ml
+++ src/ftest.ml
@@ -1,7 +1,7 @@
 open Gfile
 open Tools
 open Ford_fulk
-(* open Money *)
+open Money
     
 let () =
 
@@ -30,39 +30,11 @@ let () =
 
   (* Open file *)
   let graph = from_file infile in
-(* 
-  (* Rewrite the graph that has been read. *)
-  let () = write_file (outfile^"0") graph in
-
-  (*Test Tools*)
-  let () = write_file (outfile^"1") (clone_nodes graph) in
-  let tout_ca = add_arc (gmap graph (fun x -> int_of_string x)) 1 2 3 in 
-  let () = write_file (outfile^"2") (gmap tout_ca (fun x -> string_of_int x)) in
-
-  (* Export the graph to a file *)
-  export graph (outfile^"3") ;
-
-  (* Create a flot graph *)
-  let flot_graph = create_flot_graph (gmap graph (fun x -> int_of_string x)) in
-  let graph_ecart = graph_ecart flot_graph in
-  export (gmap graph_ecart (fun x -> string_of_int x)) (outfile^"4");
-  
-    (* Find a path *)
-  let path = (find_path graph_ecart 0 4) in
-  let rec string_path path = 
-    match path with
-    | [] -> ""
-    | x:: rest -> string_of_int x ^ " " ^ string_path rest in
-  Printf.printf "Path: %s\n" (string_path path);
-  let min_path = min_path_value graph_ecart path in
-  Printf.printf "Min path: %d\n" min_path;
-  let new_graph = update_path graph_ecart path min_path in
-  export (gmap new_graph (fun x -> string_of_int x)) (outfile^"5"); *)
 
   export graph "graph";
   ford_fulkerson (gmap graph (fun x -> int_of_string x)) _source _sink Gfile.export;
 
-  (* tricount ; *)
+  tricount ;
 
   ()
 
